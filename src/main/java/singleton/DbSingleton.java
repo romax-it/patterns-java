17d58d593af99495984edddb262c9ffdde5e403e
cromero82 <soportecarlosromero@gmail.com>
2021-08-05 12:34:29 -0500

    singleton con lazyload y threadsafe

--- src/main/java/singleton/DbSingleton.java
+++ src/main/java/singleton/DbSingleton.java
@@ -1,10 +1,24 @@
 package singleton;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbSingleton {
     private static volatile DbSingleton instance = null;
+    private static volatile Connection conn;
+
     private  DbSingleton () {
+        try {
+            DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        if(conn != null){
+            throw new RuntimeException("Use getCOnnection() method to create");
+        }
         if(instance != null){
-            throw new RuntimeException("Use getInstance() method to create");
+            throw new RuntimeException("Use getCOnnection() method to create");
         }
     }
 
@@ -16,4 +30,22 @@ public class DbSingleton {
         }
         return instance;
     }
+
+    public Connection getCOnnection(){
+        if(conn == null){
+            synchronized (DbSingleton.class){
+                if(conn == null) {
+                    try {
+                        //String dbUrl = "jdbc:derby:memory:codejava/webdb;create=true";
+                        String dbUrl = "jdbc:postgresql://127.0.0.1:5432/logogenial_bd", "romerito-admin", "f4ast3rv3rs10n*";
+                        conn = DriverManager.getConnection(dbUrl);
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return conn;
+    }
+
 }
--- src/main/java/singleton/DbSingletonDemo.java
+++ src/main/java/singleton/DbSingletonDemo.java
@@ -1,10 +1,28 @@
 package singleton;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DbSingletonDemo {
     public static void main(String[] args) {
         DbSingleton instance =  DbSingleton.getInstance();
-        System.out.println(instance);
-        DbSingleton otherInstance = DbSingleton.getInstance();
-        System.out.println(otherInstance);
+        Connection conn = instance.getCOnnection();
+
+        Statement sta;
+        try {
+            sta = conn.createStatement();
+            int count = sta.executeUpdate("create table Address(ID INT, StreetName VARCHAR(20), City VARCHAR (20))");
+            System.out.println("Table created.");
+            sta.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        long timeBefore = System.currentTimeMillis();
+        conn = instance.getCOnnection();
+        long timeAfter = System.currentTimeMillis();
+        System.out.println(timeAfter - timeBefore);
+
     }
 }
